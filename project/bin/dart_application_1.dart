c7d0fd3897f26724890ef4b5f44b445374a728f1
Jones Seiji Kadomoto Bezerra <jseiji@sbt.com.br>
2022-11-07 13:45:00 -0300

    conhecendo a orientação a objetos

---
+++ project/bin/dart_application_1.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_local_variable
+
+void main(List<String> arguments) {
+  String name = "Laranja";
+  double weight = 100.2;
+  String color = "Verde e amarela";
+  String flavor = "Doce e cítrica";
+  int daysSinceHarvest = 40;
+  bool isMature;
+
+  daysSinceHarvest >= 30 ? isMature = true : isMature = false;
+}
