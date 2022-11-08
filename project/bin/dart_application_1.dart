b723847429771d923b5b037b371b8c7b36f9dae8
Jones Seiji Kadomoto Bezerra <jseiji@sbt.com.br>
2022-11-08 11:25:34 -0300

    adiciondo parâmetros

--- project/bin/dart_application_1.dart
+++ project/bin/dart_application_1.dart
@@ -5,12 +5,22 @@ void main(List<String> arguments) {
   double weight = 100.2;
   String color = "Verde e amarela";
   String flavor = "Doce e cítrica";
-  int daysSinceHarvest = 29;
+  int daysSinceHarvest = 29; //args
   bool isMature = funcIsMature(daysSinceHarvest);
 
   print(isMature);
+  maturityStatus(fruit: name, days: daysSinceHarvest, color: "Azul");
 }
 
-bool funcIsMature(int days) {
+bool funcIsMature(int days /* params */) {
   return days >= 30 ? true : false;
+}
+
+void maturityStatus(
+    {required String fruit, required int days, String color = "sem cor"}) {
+  days >= 30
+      ? print("A $fruit está madura")
+      : print("A $fruit não está madura");
+
+  print("A $fruit é $color");
 }
